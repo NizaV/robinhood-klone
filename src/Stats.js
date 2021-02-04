7e93077d4fea594599706e1e94a468e4e1284938
NizaV <nizavera95@gmail.com>
2021-02-03 22:42:54 -0500

    connected finnhub api and started StatsRow file

--- src/Stats.js
+++ src/Stats.js
@@ -1,12 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import './css/Stats.css'
+import axios from "axios";
+import StatsRow from './StatsRow'
+
+const TOKEN = "c0dm82n48v6sgrj3a0cg";
+const BASE_URL = "https://finnhub.io/api/v1/quote";
 
 function Stats() {
 
     const [ stockData, setStockData ] = useState([]);
 
+    const getStockData = (stock) => {
+        return axios
+        .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+        .catch((error) => {
+            console.error("Error", error.message);
+        });
+    };
+
     useEffect(() => {
-        
+        let testData = []
+        const stockslist = ["AAPL", "MSFT", "TSLA", "FB", "BABA", "UBER", "DIS", "SBUX"];
+        let promises = [];
+        stockslist.map((stock) => {
+            promises.push(
+                getStockData(stock)
+                .then((res) => {
+                    testData.push({
+                        name: stock,
+                        ...res.data
+                    });
+                })
+            )
+        });
+
+        Promise.all(promises).then(() => {
+            setStockData(testData);
+        })
     }, [])
 
     return (
@@ -17,7 +47,15 @@ function Stats() {
                 </div>
                 <div className="stats__content">
                     <div className="stats_rows">
-
+                        {stockData.map((stock) => (
+                            <StatsRow
+                                key={stock.data.ticker}
+                                name={stock.data.ticker}
+                                openPrice={stock.info.o}
+                                volume={stock.data.share}
+                                price={stock.info.c}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div className="stats__header">
---
+++ src/StatsRow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function StatsRow() {
+    return (
+        <div className="row">
+            <div className="row__intro">
+                <h1>AAPL</h1>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default StatsRow
