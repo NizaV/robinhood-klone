3bf2d16bbb23f34bff8e9a342fbf40280d4c9d45
NizaV <nizavera95@gmail.com>
2021-02-10 18:26:24 -0500

    style changes

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -3,7 +3,9 @@ import './css/Newsfeed.css'
 import LineGraph from './LineGraph'
 import TimeLine from './TimeLine'
 import Chip from '@material-ui/core/Chip'
-import { Avatar } from "@material-ui/core";
+import { Avatar } from "@material-ui/core"
+import {buyStock} from "./StatsRow"
+import { db } from './firebase'
 
 function Newsfeed() {
     const [ popularTopics, setPopularTopics ] = useState([
@@ -19,7 +21,23 @@ function Newsfeed() {
         "Pharma",
       ]);
 
-      
+    //   const totalValue = () => {
+    //     const startValue = "256,789";
+    //     const boughtStock = () => {
+    //         db.collection('myStocks')
+    //                 .doc(doc.id)
+    //                 .update({
+    //                     shares: doc.data().shares+=1
+    //                 })
+    //                 db.collection('myStocks').on('shares', (snapshot) => {
+    //                     const data = snapshot.val();
+    //                     updateStarCount(postElement, data);
+    //                   });
+    //     }
+    //     if (buyStock) {
+    //         return startValue += 
+    //     }
+    //   }
 
     return (
         <div className="newsFeed">
@@ -37,7 +55,7 @@ function Newsfeed() {
                 </div>
                 <div className="newsfeed__buying__section">
                     <h2> Buying Power</h2>
-                    <h2>$4.11</h2>
+                    <h2>$768.56</h2>
                 </div>
                 <div className="newsfeed__market__section">
                     <div className="newsfeed__market__box">
